042977f070fba5f27dd0b01d3865ad0892c32bec
aalapan <aalapan@my.centennialcollege.ca>
2020-10-25 22:25:41 -0400

    update

--- server/config/db.js
+++ server/config/db.js
@@ -1,6 +1,5 @@
 module.exports =
 {
-    //"URI": "mongodb://localhost:27017/business_list"
-    "URI": "mongodb+srv://aalapan:k1NWyIi9LWxza56T@databasemongo.60wdf.mongodb.net/business_list?retryWrites=true&w=majority"
+   "URI": "mongodb+srv://aalapan:Immanuel@cluster0.gitec.mongodb.net/businesscontacts-list?retryWrites=true&w=majority"
 }
 module.exports.Secret = "SomeSecret";
